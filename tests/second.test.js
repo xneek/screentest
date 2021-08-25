1feb51a20986579955cc4638e16ac0ae935fb16c
AlexeySaraev <89474939+AlexeySaraev@users.noreply.github.com>
2021-08-25 07:59:17 +0300

    Update second.test.js

--- tests/second.test.js
+++ tests/second.test.js
@@ -9,10 +9,18 @@ describe('jest-image-snapshot usage with an image received from puppeteer', () =
 
   it('works', async () => {
     const page = await browser.newPage();
-    await page.goto('https://google.com');
+    await page.goto('https://kubernetes.shtormtech.ru/b2x-dev1/app/client/1-1');
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.evaluate(async() => {
+        await new Promise(function(resolve) { 
+           setTimeout(resolve, 1000)
+        });
+   });
     const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customDiffConfig: { threshold: 0.7 }
+    });
 
-    expect(image).toMatchImageSnapshot();
   });
 
   afterAll(async () => {
